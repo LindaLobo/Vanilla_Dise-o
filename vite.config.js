2aa38b74603291b31b0fab8c1fc2e23a0299ecf7
Linda Aguilar <lindalobo.aguilar@gmail.com>
2024-06-19 14:11:20 -0400

    Update url context

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://LindaLobo.github.io/Vanilla_Dise-o/'
+  // base:'https://LindaLobo.github.io/Vanilla_Dise-o/'
 })
